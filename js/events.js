e2e296e33093d3e05a6707fad15ae649022dec54
vicfriedman <victoria.m.friedman@gmail.com>
2016-01-21 12:19:37 -0500

    remove solution

--- js/events.js
+++ js/events.js
@@ -1,34 +1,5 @@
 //define functions here
-function getIt(){
-  $('p').on("click", function(){
-    alert("HEY")
-  });
-}
-
-function frameIt(){
-  $('img').on('load', function(){
-    $('img').addClass("tasty");
-  });
-}
-
-function pressIt(){
- $('#typing').on('keydown', function(theKey) {
-    if(theKey.keyCode == 71){
-        alert('g was pressed');
-    }
-  });
-}
-
-function submitIt(){
-  $("form").on("submit", function() {
-    alert('your form is going to be submitted now');
-    return;
-  });
-}
 
 $(document).ready(function(){
-  getIt();
-  frameIt();
-  pressIt();
-  submitIt();
+// call functions here
 });
