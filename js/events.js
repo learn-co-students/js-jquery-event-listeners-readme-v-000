45c26160b5b39c8a4c7a01b68d20f893bddb6cef
macgeek30 <mike25ko@gmail.com>
2016-02-03 15:43:50 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,42 @@
 //define functions here
 
+
 $(document).ready(function(){
 
-// call functions here
+  getIt();
+
+  frameIt();
+
+  pressIt();
+
+  submitIt();
 
 });
+
+
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $("input#typing").on('keydown', function(key) {
+    if (key.keyCode == 71) {
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("your form is going to be submitted now");
+    return;
+  });
+}
