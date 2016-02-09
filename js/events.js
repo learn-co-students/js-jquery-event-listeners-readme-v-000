966ac99bda1c3403085aeb2a94f45702580d532a
Karuna <imkaruna@gmail.com>
2016-02-08 20:30:28 -0500

    Done Event Listener Lab

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+
+    alert('Hey!');
+  });
+}
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key){
+    if(key == 34){
+      alert('You pressed key G');
+    }
+  });
+
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('your form is going to be submitted now');
+    return;
+  });
+
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
