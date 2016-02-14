def72c4a793d691c6db0509f17960c25528d6630
shmulim <shmuel.markel@gmail.com>
2016-02-14 15:51:18 -0800

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt(){
+  jQuery('p').on('click', function(){
+    alert('Hey!');
+  });
+}
+
+function frameIt(){
+  jQuery('img').on('load', function(){
+    jQuery('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  jQuery('input').on('keydown', function(key){
+    if (key.keyCode == 71){
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt(){
+  jQuery('form').on('submit', function(){
+    console.log('your form is going to be submitted now');
+    return;
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
