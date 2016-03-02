f86949ada2eded08bc55b46c723505d08ccea612
Shana L Moore <shana.lavina.moore@gmail.com>
2016-03-02 13:38:52 -0800

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,30 @@
 //define functions here
+function getIt () {
+  $('p').on("click", function () {
+    alert('hey!');
+  })
+}
+
+function frameIt () {
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+ $('#typing').on('keydown', function(theKey) {
+    if(theKey.keyCode == 71){
+        alert('g was pressed');
+    }
+  });
+}
+
+function submitIt () {
+  $('form').on('submit', function  () {
+    alert("your form is going to be submitted now");
+    return;
+  })
+}
 
 $(document).ready(function(){
 
