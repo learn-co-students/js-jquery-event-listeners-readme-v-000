b5614a7e561591f96d9fd00c026eff659ef1a14e
dannyd4315 <dannyd4315@gmail.com>
2016-03-10 13:51:09 +1100

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt () {
+  $('p').on('click', function() {
+    alert('Hey!')
+  });
+}
+
+function frameIt () {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  });
+}
+
+function pressIt () {
+  $('#typing').on('keydown', function(key) {
+    if(key.keyCode == 71){
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt () {
+  $("form").on("submit", function() {
+    alert('your form is going to be submitted now');
+    return;
+  });
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
