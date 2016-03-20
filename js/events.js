0d3383a465681bbabcdd0d2f46faa3a6507c8a53
Adam King <adam.king0126@gmail.com>
2016-03-20 17:36:29 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,29 +1,33 @@
 function getIt() {
-  alert("Hey!");
+  $("p").on("click", function(){
+    alert("Hey!");
+  });
 }
 
 function frameIt() {
+  $('img').on("load", function(){
   $('img').addClass('tasty');
+});
 }
 
-function pressIt(key) {
-  if(key.keyCode == 71){
-    alert("g key was pressed.");
-  }
+function pressIt() {
+  $(document).on("keydown", function(key) {
+    if(key.keyCode == 71){
+      alert("g key was pressed.");
+    }
+  });
 }
 
 function submitIt() {
+  $("form").on("submit", function(){
   alert("your form is going to be submitted now");
   return;
+});
 }
 
 $(document).ready(function(){
-  $("p").on("click", getIt);
-  $("img").on("load", frameIt);
-
-  $(document).on('keydown', function(key) {
-    pressIt(key);
-  });
-
-  $("form").on("submit", submitIt);
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
