630b24f47b60afd409269a1e30d89ddca2c7f23b
Adam King <adam.king0126@gmail.com>
2016-03-20 17:01:30 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
-//define functions here
+function getIt() {
+  alert("Hey!");
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+}
+
+function pressIt(key) {
+  if(key.keyCode == 71){
+    alert("g key was pressed.");
+  }
+}
+
+function submitIt() {
+  alert("your form is going to be submitted now");
+  return;
+}
 
 $(document).ready(function(){
+  $("p").on("click", getIt);
+  $("img").on("load", frameIt);
 
-// call functions here
+  $(document).on('keydown', function(key) {
+    pressIt(key);
+  });
 
+  $("form").on("submit", submitIt);
 });
