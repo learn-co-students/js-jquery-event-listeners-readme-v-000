d0d5bc2588d3d15278322705a3766a5441f3de05
empireofryan <rjohnson2001@gmail.com>
2016-03-24 16:46:22 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on("click", function() {
+    alert('Hey!');
+  })
+}
 
-// call functions here
+function frameIt() {
+  $('img').on("load", function() {
+    $(this).addClass("tasty");
+  })
+}
 
+function pressIt() {
+  $('input').on("keydown", function(key) {
+    if (key.keyCode = 71) {
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("your form is going to be submitted now");
+    return;
+  });
+}
+
+$(document).ready(function() {
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
