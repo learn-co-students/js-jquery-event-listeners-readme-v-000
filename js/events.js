e639e9b1c7c096b187fd16bce13a72cf69211385
bacitracin <tracetrace@gmail.com>
2016-03-25 13:30:45 -0700

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+    $('img').css({'border-color':'red'});
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown', function(){
+    if(key.keyCode == 103){
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('your form is going to be submitted now');
+    return;
+  });
+}
+
+
+
+
 
 $(document).ready(function(){
-
-// call functions here
 
 });
