12b89db108f5436fdf47098c22372e9169fb4e83
Rawradin <edgar.rawr@gmail.com>
2016-03-28 16:26:23 -0700

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+var getIt = function(){
+  $('p').on('click',function(){
+    alert("Hey!");
+  })
+}
 
+var frameIt = function(){
+  $('img').on('load',function(){
+    $(this).addClass('tasty');
+  })
+}
+var pressIt = function(){
+  $('input').on('keydown', function(key) {
+    if(key.keyCode == 71){
+        alert('g was pressed');
+    }
+  });
+}
+
+var submitIt = function(){
+  $('form').on('submit',function(){
+    alert('your form is going to be submitted now');
+    return;
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
