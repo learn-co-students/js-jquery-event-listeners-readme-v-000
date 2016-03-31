89f19b4c096c03daedcd0598046d160bc8863818
zachnewburgh <zach.newburgh@gmail.com>
2016-03-31 10:28:11 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
 
-// call functions here
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
 
-});
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.keyCode === 67) {
+      alert("You have pressed g.");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("your form is going to be submitted now");
+  });
+}
