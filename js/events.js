25d619dcf9b288447accad429792a5884151e5dc
mmmeyers <marsi286@gmail.com>
2016-04-05 13:12:33 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    return "Hey!";
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    return $('img').addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $('input#typing').on('keydown', function(key) {
+    if (key.keyCode === 71) {
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('your form is going to be submitted now');
+    return;
+  });
+}
+
+
 $(document).ready(function(){
 
 // call functions here
+
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
