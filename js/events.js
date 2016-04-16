c9f07d89b265e260ac8029ae0592028ab0489f45
febbraiod <donedesign1@gmail.com>
2016-04-16 12:12:46 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+    });
+}
+
+function pressIt(){
+  $('input').on('keydown', function(key){
+      if(key.keyCode == 71){
+        alert('you pressed g');
+      }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('your form is going to be submitted now');
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
