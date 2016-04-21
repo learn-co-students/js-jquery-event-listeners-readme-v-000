9a53dfcbc42938d1a4c16b0bb2c1f53a83aacb09
jsgoldb <jgoldb@yahoo.com>
2016-04-21 10:37:57 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key){
+    if(key.keyCode === 71){
+      alert('You pressed g!');
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert('your form is going to be submitted now');
+    return;
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
