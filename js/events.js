fe184a15cd12290995cceb17b786d565f8f9aaef
cernanb <cernanb@gmail.com>
2016-05-04 09:52:18 -0700

    added jquery event listeners

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+}
 
-// call functions here
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  });
+}
 
-});
+function pressIt(){
+  $('#typing').on('keydown', function(key){
+    if(key.keyCode == 103){
+      alert('g key was pressed');
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('your form is going to be submitted now');
+    return;
+  });
+}
