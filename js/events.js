661cccc04bac9fd83d887dd917866e5019c27a88
Brad1974 <danieltbrad@gmail.com>
2016-05-11 10:31:06 -0700

    done

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.keyCode == 103){
+      alert('g was pressed');
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    if ($("input:first" ).val() === "I love jQuery") {
+      alert('your form is going to be submitted now');
+      return;
+    }
+  })
+}
+
+// Define a function submitIt that does not accept a parameter. The function should alert "your form is going to be submitted now". And then return;.
+
 
 $(document).ready(function(){
-
-// call functions here
 
 });
