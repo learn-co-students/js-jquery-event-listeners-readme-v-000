697f38d3589c219c202a6de9cf9959e819a4a5a3
Lady Codington <myemailforcoding@gmail.com>
2016-05-15 08:59:49 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,39 @@
 //define functions here
+function getIt() {
+  $( 'p' ).on( 'click', notify );
+}
 
-$(document).ready(function(){
+function notify() {
+  alert( 'Hey!' );
+}
+
+function frameIt() {
+  $( 'img' ).on( 'load', addTasty );
+}
+
+function addTasty() {
+  $( 'img' ).addClass( 'tasty' );
+}
+
+function pressIt() {
+  $( 'input' ).on( 'keydown', function( key ) {
+    if( key.keyCode == 71 ) {
+      alert( 'g was pressed' );
+    }
+  });
+}
+
+function submitIt() {
+  $( 'form' ).on( 'submit', function () {
+    if ( $( 'input:first' ).val() === 'I love jQuery' ) {
+      alert( "your form is going to be submitted now" );
+      return;
+    }
+    alert( 'you entered the wrong value' );
+  });
+}
+
+$( document ).ready( function(){
 
 // call functions here
 
