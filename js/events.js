781986f563d7fb5bce2e6403caaf5f460b748dd6
Tom Johns <tommyskies@hotmail.com>
2016-05-26 20:22:38 -0600

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,43 @@
 
 $(document).ready(function(){
 
-// call functions here
-
+	getKeyCode();
+	getIt();
+	frameIt();
+	pressIt();
+	submitIt();
+	
 });
+
+function getKeyCode(){
+	$(document).on('keypress', function(key) {
+		console.log("" + key.keyCode);
+  });
+}
+
+function getIt(){
+	$('p').on('click', function(){
+		alert("Hey!");
+	});
+}
+
+function frameIt(){
+	$('img').on('load', function(){
+		$(this).addClass('tasty');
+	});
+}
+
+function pressIt(){
+	$('input').on('keydown', function(key){
+		if (key.keyCode == 103){
+			alert("you pushed 'g'")
+		}
+	});
+}
+
+function submitIt(){
+	$('form').on("submit", function(){
+		alert("your form is going to be submitted now");
+		return;
+	});
+}
