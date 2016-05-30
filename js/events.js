5cf612402ff1354eb0187ef442bcb3ca38259280
LKWLaLa <lkwlala@gmail.com>
2016-05-30 17:43:22 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(g) {
+    if (g.which === 71) {
+      alert("The key you pressed is g!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
+
+
+
+
 $(document).ready(function(){
 
-// call functions here
+
 
 });
