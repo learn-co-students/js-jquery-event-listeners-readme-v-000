8f4b1bac794712ec4e9f4f4eed1a7398181bef61
McN33ly <rmcneely23@gmail.com>
2016-06-02 12:36:11 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,29 @@
 $(document).ready(function(){
 
 // call functions here
-
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which === 71) {
+      alert("You pressed 'g'!");
+    }
+  })
+};
