35099e06071a4c85c001efa6c8efcd286c8c30f8
Adam Dorsky <ebshimon@gmail.com>
2016-06-09 03:59:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){alert('hey!');})
+}
+
+function frameIt() {
+  $('img').on('load', function(){$('img').addClass('tasty');})
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {alert('Your form is going to be submitted now.');
+})
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key){
+    if(key.which == 71){alert("g was pressed");}
+  })
+}
+
+
 
 $(document).ready(function(){
-
-// call functions here
+  getIt();
+  frameIt();
+  pressIt();
 
 });
