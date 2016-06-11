3dac05b526d98e2955d8ca0d65e5c6e807a3ed78
James Novak <jamesnovak90@gmail.com>
2016-06-11 15:28:03 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.keyCode == 71) {
+      alert('You pressed G');
+    }
+  });
+}
+
+$(document).ready(function(){
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 // call functions here
 
 });
