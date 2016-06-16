282281a0ff38ec505c9214d1b9e2afa6cca5f4c8
corya0687 <corya0687@gmail.com>
2016-06-16 15:58:33 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,17 @@
 
 $(document).ready(function(){
 
-// call functions here
-
 });
+
+function getIt() {
+  $('p').on('click',function () {
+    alert("Hey!")
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+
+  });
+
+}
