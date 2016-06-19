c20b09209fafe849fe66f586b679b9db813a7cfb
Christopher Bruen <bruen.chris@gmail.com>
2016-06-19 00:39:17 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+  });
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on("load", function() {
+      $('img').addClass('tasty');
+  });
+}
 
-// call functions here
+function pressIt() {
+  $('#typing').on("keydown", function(key) {
+      if (key.which == 71) {
+        alert("g was pressed");
+      }
+  });
+}
 
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+$(document).ready(function() {
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
