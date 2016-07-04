d243854229b4544ab22c96e9a93594d61e357400
Drew Pham <drewvpham@gmail.com>
2016-07-04 02:19:06 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
-//define functions here
+
+function getIt() {
+  $('p').on('click', function(){
+    alert('hey');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key){
+    if(key.keyCode == 71) {
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
+
 
 $(document).ready(function(){
+  getit();
+  frameIt();
+  pressIt();
+  submitIt();
 
-// call functions here
 
 });
