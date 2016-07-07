cc1b3f909a97933a5b8c75b5a1bdbdc24ae31f4f
cassaram09 <cassaram09@gmail.com>
2016-07-07 19:57:36 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
-//define functions here
+function getIt(){
+   $("p").on("click", function(){
+    alert("Hey!");
+    return;
+  });
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $(this).addClass("tasty");
+    return;
+  });
+}
+
+function pressIt(){
+  $("#typing").on('keydown', function(key){
+    if (key.which == 71){
+      alert("You pressed the G key");
+      return;
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+    return;
+  });  
+}
 
 $(document).ready(function(){
 
-// call functions here
+  getIt();
 
+  frameIt();
+
+  pressIt();
+
+  submitIt();
 });
