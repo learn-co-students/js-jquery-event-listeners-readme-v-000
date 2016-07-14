a0818d77d144daf38e4acc6da73eb428043c7de1
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-13 21:49:59 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
+  // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
+
+  function getIt() {
+    $('p').on("click", function(){
+      alert("Hey!");
+    });
+  }
+
+  function frameIt() {
+    $('img').on("load", function(){
+      $('img').addClass("tasty");
+    });
+  }
+
+  function pressIt() {
+    $(document).on("keydown", function(key) {
+      if(key.which === 71){
+        alert('s was pressed');
+      }
+    });
+  }
+
+  function submitIt() {
+    $('form').on("submit", function() {
+      alert("Your form is going to be submitted now.");
+    });
+  }
