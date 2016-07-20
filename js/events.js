3c52884e73f9e0e053b5a8d182dd0056effa10b8
hudbuz <hzb2@cornell.edu>
2016-07-20 01:39:34 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,41 @@
 //define functions here
 
-$(document).ready(function(){
+//define functions here
+function getIt() {
 
-// call functions here
+ $('p').on('click', function() {
+  return alert("Hey!");
+ });
+}
+function frameIt() {
 
-});
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.keyCode == 71) {
+      alert("Aintt nothing but a g thang babby")
+    }
+  })
+
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert("Your input will be submitted.")
+    
+  })
+
+}
+
+$(document).ready(function() {
+ 
+
+   getIt();
+   frameIt();
+   pressIt();
+  submitIt();
+}); 
