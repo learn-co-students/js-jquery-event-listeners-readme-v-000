6564cb1a90821703d0a9b15cecd406ad1f933bb3
Gandhi Anderson <thegands@gmail.com>
2016-07-23 00:30:06 -0600

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert ('hey!');
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('g was pressed');
+    }
+  })
+}
+
+function submitIt () {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
 
 $(document).ready(function(){
 
