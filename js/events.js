fd90e66db4c4fb4f57bbc315367b4221b7a05604
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-07 16:09:06 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
-//define functions here
+  function getIt() {
+    $('p').on('click', function() {
+      alert("Hey!")
+    })
+  }
+
+  function frameIt() {
+    $('img').on('load', function() {
+        return $('img').addClass('tasty');
+    })
+  }
+
+  function submitIt() {
+    $('form').on('submit', function() {
+      alert("Your form is going to be submitted now.");
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+      if(key.which == 71){
+        alert("Your form is going to be submitted now.");
+      }
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
 
 });
