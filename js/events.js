5fe9f1369b20e1943a1e63cad3be28ce26dcea21
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-08-25 14:34:36 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt(){
 
 function pressIt(){
   $("input").on("keydown", function(key){
-    if(key.keyCode === 71){
+    if(key.which === 71){
       alert("You pressed G!");
     }
   })
--- test/events-test.js
+++ test/events-test.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const expect = require('expect')
 const fs = require('fs')
 const jsdom = require('jsdom')
