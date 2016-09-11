788bbbecced060f2633d9be98feadb158d1a59cd
andrewstone8 <andrewstone8@gmail.com>
2016-09-11 15:52:46 -0700

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt() {
+  $('p').click(function(){
+    alert("Hey!");
+  });
+};
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+};
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which === 71) {
+      alert("You pressed the g key down.");
+    }
+  });
+};
+
+function submitIt() {
+  $("form").on('submit', function() {
+      alert('Your form is going to be submitted now.')
+  });
+};
 
 $(document).ready(function(){
 
-// call functions here
+  getIt()
+
+  frameIt()
 
 });
