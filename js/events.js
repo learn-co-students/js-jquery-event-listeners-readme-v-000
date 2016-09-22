006cf4cabc576f50a789e9323f3d7960ba80c5ce
kentleong <kentleong@github.com>
2016-09-22 03:25:46 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+  })
+}
 
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on("keydown", function(key) {
+    if (key.which === 71){
+      alert("you pressed the g key")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
