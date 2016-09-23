e06c435a1b64a7d5ecb5425cfcd1fd252294e405
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-23 14:37:03 -0400

    learn instructor help

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
 
+function getIt() {
+  alert('Hey!');
+}
+
+function frameIt() {
+  $('img').addClass("tasty");
+}
+
+function pressIt() {
+  if ($('#typing').val() == 'g') {
+    return alert('g key pressed');
+  }
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now.");
+}
+
 $(document).ready(function(){
-
+  debugger;
 // call functions here
-
+  $('p').on("click", getIt());
+  $('img').on("load", frameIt());
+  $('#typing').on("keydown", pressIt());
+  $('form').on("submit", submitIt());
 });
