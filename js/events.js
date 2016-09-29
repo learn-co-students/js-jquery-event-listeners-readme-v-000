90d784dc8574fcbaa7fd2b12346053159902a3bf
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-09-29 16:43:50 -0700

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt(){
+	$('p').on("click", function(){
+		alert('Hey!');
+	});
+}
+
+function frameIt(){
+	$('img').on("load", function(){
+		$('img').addClass("tasty");
+	});
+}
+
+function submitIt(){
+	$('form').on("submit", function(){
+		alert('Your form is going to be submitted now.');
+	});
+}
+
+function pressIt(){
+	$('input').on("keydown", function(key){
+		if (key.which === 71){
+			alert('the g key has been pressed')
+		}
+	});
+}
 
 $(document).ready(function(){
 
