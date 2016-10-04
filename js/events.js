41ddbffc214e9d82af25bc2a45f737b36c7b2e13
kaileighrose <kaileighrose@gmail.com>
2016-10-03 20:58:27 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt() {
+  $("p").on('click', function() {
+      alert('hey');
+});
+}
+
+function frameIt() {
+  $(document).on('load', function() {
+    $("img").addClass('tasty');
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+    return;
+});
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('g was pressed');
+  }
+});
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+submitIt();
+pressIt();
 });
