6d26d72e922c58547a3191aaa66e13a37e0c89c0
dannyenfp <daniel.valmas@gmail.com>
2016-10-05 11:10:59 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,3 +1,30 @@
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $("input").on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+
 //define functions here
 
 $(document).ready(function(){
