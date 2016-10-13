f481981505cec442bfc0a4bcdf72f3438070d06c
bharti gurnaney <bharti0717@yahoo.com>
2016-10-13 16:39:43 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt()
+{
+  $('p').on("click", function(key){
+  alert("Hey!");
+});
+ }
+
+function frameIt()
+{
+  $('img').on("load", function(key){
+  $('img').addClass('tasty');
+});
+}
+
+function pressIt()
+{
+  $(window).on("keydown", function(key){
+    if(key.which==71)
+       alert("you pressed g!");
+}) ;
+}
+
+function submitIt()
+{
+  $('form').on("submit", function(key){
+  alert("Your form is going to be submitted now.");
+});
+}
+
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+pressIt();
+frameIt();
+submitIt();
 });
