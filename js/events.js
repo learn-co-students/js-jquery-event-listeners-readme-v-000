66e8f7ac5cddd4518f408eb1f7132a27193f9f34
Logan McAnsh <l.m@loganmcansh.com>
2016-10-25 18:09:50 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').click(function() {
+    window.alert("Hey!");
+  });
+}
 
-// call functions here
+function frameIt() {
+  $('img').load(function() {
+    $(this).addClass("tasty");
+  });
+}
 
+function pressIt() {
+  $('input').keydown(function(key){
+    if(key.which == 71) {
+      window.alert("g key pressed");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').submit(function() {
+    window.alert("Your form is going to be submitted now.")
+  });
+}
+
+
+$(document).ready(function() {
+
+  // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
