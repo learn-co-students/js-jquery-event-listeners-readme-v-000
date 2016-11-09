aee4f29e8a0fca857dea435d4c0a7877d5486d94
lenn hypolite <lennhydesign@gmail.com>
2016-11-09 13:18:01 -0500

    Done

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $(document).on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+   alert("Your form is going to be submitted now.");
+ });
+}
+
+function pressIt(){
+  $("#typing").on("keydown", function(key){
+    if ( key.which === 71){
+      alert("g was pressed");
+    }
+  })
+}
 
 $(document).ready(function(){
-
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 // call functions here
 
 });
