0a25c6eab1f482525b8932db31567a34e9b046b9
bedle88 <robey.mc@gmail.com>
2016-11-22 10:53:43 -0500

    done

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $(document).on("load", function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $(document).on("keydown", function(key) {
+    if (key.which == 71) {
+      alert('g was pressed');
+      return;
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+  frameIt()
 
 });
