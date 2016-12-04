f056348bfb1dca021156e3c622f11c0a80fa2ceb
williamjones62 <williamjones62@gmail.com>
2016-12-03 19:57:28 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,32 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    // 'g' corresponds to 71
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
