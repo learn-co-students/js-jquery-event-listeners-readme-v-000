ffcc6cf3290023e71736c1aa99e1da654ea5165d
Yechiel K <chilikkal@gmail.com>
2016-12-08 21:15:03 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,41 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which == 71){
+        alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
+  getIt();
+
+  frameIt();
+
+  pressIt();
+
+  submitIt();
+
 
 });
