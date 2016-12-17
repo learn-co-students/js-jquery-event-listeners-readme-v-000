99552a5d3f7af95d7ffa5efa331f7ad88455383b
bshap93 <bshap93@gmail.com>
2016-12-17 06:24:35 -0700

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,37 @@
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
 
+frameIt();
+
+pressIt();
 });
+
+
+function getIt(  ) {
+	$('p').on("click", function() {
+		alert("Hey!");
+	})
+}
+
+function frameIt() {
+	$('img').on("load", function() {
+		$('img').addClass("tasty");
+	})
+} 
+
+
+function submitIt() {
+	$("form").on("submit", function() {
+		alert("Your form is going to be submitted now.");
+	})
+}
+
+function pressIt() {
+	$("input#typing").on("keydown", function(key) {
+		if (key.which == 71) {
+			alert("g was pressed");
+		}
+	})
+}
