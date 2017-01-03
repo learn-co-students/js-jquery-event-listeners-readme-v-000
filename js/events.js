9b4fe075c23cecf2dc23d37560ff5d8b1f6a7b59
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-01-03 12:58:07 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
+  getIt();
+  frameIt();
 
 });
+
+function getIt() {
+  $('p').on("click", function(){
+    alert('Hey!');
+  });
+};
+
+function frameIt() {
+  $('img').on("load", function() {
+    $(this).addClass("tasty");
+  });
+};
+
+function pressIt() {
+  $('input#typing').on("keydown", function(key) {
+    if (key.which == 71) {
+      alert('You pressed g');
+    };
+  });
+};
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+};
