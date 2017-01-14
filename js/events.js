d65a3afd145ccdefa8fe5a32ecd64a1b59ff876a
Emmanuel Owusu <e_owusu@live.com>
2017-01-14 22:43:32 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,28 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  })
+}
+
+function pressIt(){
+  $("#typing").keydown("keydown", function(key){
+    if(key.which === 71)
+    alert("You pressed 'g'!");
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(form){
+    alert("Your form is going to be submitted now.");
+  })
+}
