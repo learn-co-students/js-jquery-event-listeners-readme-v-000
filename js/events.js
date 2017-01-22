ac8143ddd30e68c3c527891bfffb12d0d14a2dd2
Joe <jsbenning4@gmail.com>
2017-01-21 23:08:59 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -4,4 +4,38 @@ $(document).ready(function(){
 
 // call functions here
 
+$('p').on('click', getIt = function() {
+    alert('Hey!');
+    return;
+})
+
+
+$('img').addClass('tasty').on('load', frameIt = function() {
+    return;
+})
+
+/* 
+$('document').on('keydown', pressIt = function(key) {
+  {
+    if (key.which == 71) {
+      alert('You pressed the 'g' key, genius...');
+    return;
+    }
+  }
+})
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
