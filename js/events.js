a9f89e4ac463578cadcf2b14e5e9a1d594658d42
johnram528 <johnram528@gmail.com>
2017-02-21 15:44:23 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,38 @@
 
 $(document).ready(function(){
 
-// call functions here
+  getIt()
 
+  frameIt()
+
+  submitIt()
+
+  pressIt()
 });
+
+
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hello")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass("tasty")
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(e) {
+    if(e.which == 71){
+      alert("G was pressed.")
+    }
+  })
+}
