69a733146c464ecd9182608d6f01042bb7fac3a2
Adam Lucia <lucia.adam@gmail.com>
2017-02-22 06:26:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!")
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function functionName(img) {
+    $(this).addClass("tasty")
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    if (parseInt(e.which) === 71) {
+      alert('Quit pressing g')
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
