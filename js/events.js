cf1972b16be4b2dd0d5158e4cae347cf3eb5c3fd
Adam Friedl <ajfriedl@gmail.com>
2017-02-26 13:11:13 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
-//define functions here
+function getIt() {
+	return $('p').on('click', function() {
+		alert("Hey!")
+	})
+}
+
+function frameIt() {
+	$('img').on('load', function() {
+		$('img').addClass('tasty')
+	})
+}
+
+function pressIt() {
+	$('#typing').on('keydown', function(key) {
+		if (key.which === 71) {
+		alert("You pressed 'g'!")
+		}	
+	})
+}
+
+function submitIt() {
+	$('form').on('submit', function() {
+		alert('Your form is going to be submitted now.')
+	})
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
+
+frameIt()
+
+pressIt()
+
+submitIt()
 
 });
+
