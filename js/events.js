2b799595fadec3c28162f9e13a4d119488e068ac
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2017-03-01 14:12:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,3 +1,18 @@
+$('p').on("click", function getIt() {
+  alert('Hey!');
+});
+
+$('img').on('load'), function frameIt() {
+  .addClass("tasty");
+}
+
+$('document').on('keydown', function pressIt() {
+  if(key.which == 71){
+    alert('g was pressed');
+  }
+});
+
+
 //define functions here
 
 $(document).ready(function(){
