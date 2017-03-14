ee52fc3a2610c495b2e3becc4a5fcbab39565a68
Jonathan Foster <redcabinny@gmail.com>
2017-03-14 13:55:38 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
 
 $(document).ready(function(){
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 // call functions here
 
 });
+
+function getIt() {
+  $('p').on("click", function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which == 71){
+        alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
