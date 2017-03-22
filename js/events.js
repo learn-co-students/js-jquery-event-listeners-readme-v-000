a2f72fc690767e89bb7311973967d75f51d8cff8
Bob Habig <rfhab2@gmail.com>
2017-03-22 19:20:26 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $('input#typing').on('keydown', function(key) {
+    if(key.which === 71) {
+      alert("You pressed 'g'")
+    }
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
 
 });
