54deb80438a25ad9f641ca8c94c1370e2a2379e6
walwoodr <walwoodr@gmail.com>
2017-04-10 17:18:59 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,36 @@
 $(document).ready(function(){
 
 // call functions here
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 
 });
+
+
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert("something")
+    }
+  })
+}
