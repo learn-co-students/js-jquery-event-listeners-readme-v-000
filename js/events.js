0c5477b1a036ca89c41a0d910ab095786f583927
Holger Paas <holger.paas@gmail.com>
2017-04-24 01:01:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt(){
+  alert('Hey!');
+}
+
+function submitIt(){
+  $('form').on('submit',function(){
+    alert('Your form is going to be submitted now.');
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown',function(e){
+    const key = parseInt(e.detail || e.which)
+    if (key == 71){
+      alert('g was pressed');
+    }
+  });
+}
+
+function frameIt(){
+  $('img').addClass('tasty');
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+$('p').on('click',getIt);
+submitIt();
+pressIt();
+// $('form').on('submit',submitIt);
+$('img').on('load',frameIt);
+//$('input').on('keydown',pressIt);
 });
