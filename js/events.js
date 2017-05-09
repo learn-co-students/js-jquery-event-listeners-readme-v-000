e70f57dc3a2480e048a75c7aef22433370d92c57
Sammy Wu <wu.sammy@gmail.com>
2017-05-09 02:45:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
-
 $(document).ready(function(){
-
-// call functions here
-
 });
+
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+    return;
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
+
+function frameIt() {
+  $("img").on("load", function () {
+    $(this).addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $("form").on("keydown", function(key) {
+    if (key.which == 71) {
+      alert("g was pressed")
+    }
+  })
+}
