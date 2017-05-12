41d9c8de5dff471af49804f0ef75ba56e88b2fa7
Sean Clarke <xseanclarke@gmail.com>
2017-05-12 13:49:24 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -22,7 +22,7 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('form').on('submit', function(e) {
+  $('form').on('submit', function() {
     alert('Your form is going to be submitted now.')
   })
 }
