9e327bb669e28705a11657c7772530a4286c6e15
kkashuda <kkashuda@gmail.com>
2017-06-03 12:18:30 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(e) {
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 });
