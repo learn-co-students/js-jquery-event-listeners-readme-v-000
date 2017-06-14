3ee70dbe9d978623d49a84583f24d7b5c798fb51
wook kim <wooke2e@gmail.com>
2017-06-14 16:16:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,24 @@
 //define functions here
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!")    
+  });
+}
+function frameIt(){
+  $('img').on("load", function(){
+  
+  })
+}
+function pressIt(){
+  
+}
+function submitI(){
+  
+}
 
 $(document).ready(function(){
+  getIt()
+  
 
 // call functions here
 
