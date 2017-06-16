012d1f3d1abcf99833fe858bde42113ca8341388
nicholasbair <bairinc@gmail.com>
2017-06-16 16:05:51 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $('p').on('click', () => { alert('Hey!'); });
+}
+
+function frameIt() {
+  $('img').on('load', () => {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', (e) => {
+    if (parseInt(e.which) === 71) {
+      alert('hey there G!');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', () => {
+    alert('Your form is going to be submitted now.');
+  });
+}
 
 $(document).ready(function(){
 
-// call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
