dcc7e91cf07bcb73f864bbe3e05b273865e23395
Howard DeVennish <howarddevennish@gmail.com>
2017-06-23 14:49:41 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,25 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+})}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+})}
+
+function pressIt() {
+  $('body').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert('You pressed G')
+    }
+})}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+})}
 
 $(document).ready(function(){
 
