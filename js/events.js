294c9afa9652f11765b70bdd2488a82792caaa8f
Parv Kumar <pkumar@trinity.edu>
2017-06-27 18:48:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,30 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+      $('img').addClass('tasty');
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  });
+}
+
+function pressIt(){
+  $('form').on('keydown',function(key){
+    if(key.which === 71){
+      alert("G was pressed.")
+    }
+  });
+}
+
 
 $(document).ready(function(){
 
