23553bf45b2de2d03cc0bd2afc16e6064a280c39
Sergio Mendoza <smendoza787@gmail.com>
2017-06-28 22:36:18 -0700

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("You pushed G");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
 
 });
