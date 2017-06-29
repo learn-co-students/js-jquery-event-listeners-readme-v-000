f5f62f91db0739f94a1fe9d7752a641ce19ab4d4
Menachem Hornbacher <me@mendel.tech>
2017-06-29 17:33:20 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt() {
+  $('p').on('click', () => {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $( 'img' ).on('load', function(){
+    var x = $(this);
+    $( this ).addClass( "tasty" );
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', () => {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown', (key) => {
+    if (key.which === 71) {
+      alert("You pressed G")
+    }
+  })
+}
+
+frameIt();
 
 $(document).ready(function(){
 
-// call functions here
+  // call functions here
+  getIt();
+  pressIt();
 
 });
