9b049eba0a07212f33f727b81136a87da678d2bf
Timothy Cevallos <timcev@gmail.com>
2017-07-06 20:11:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+});
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $("input").on('keydown', function(key) {
+    if(key.which == 71){
+        alert('G was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function(e) {
+    alert('Your form is going to be submitted now.');
+});
+}
 
 $(document).ready(function(){
-
+  getIt()
+submitIt()
+pressIt()
 // call functions here
 
 });
