2cc35cbc7a40ac64c10d3d1bd89335e605043e0b
Elias Ventura <eliasventura23@gmail.com>
2017-07-29 04:03:14 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,11 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt() {
+  $('p').on("click", function(key){
+    if(key.which= 80){
+      alert('p was pressed')
+    }
+  })
+}
