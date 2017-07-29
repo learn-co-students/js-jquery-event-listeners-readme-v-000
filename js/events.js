2744d6ce2ae259a3bbab54d4db57ee0b129d8439
Elias Ventura <eliasventura23@gmail.com>
2017-07-29 04:23:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -13,3 +13,23 @@ function getIt() {
     }
   })
 }
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+  if(key.which == 71){
+      alert('g was pressed');
+  }
+})
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+      alert('Your form is going to be submitted now.');
+    })
+  };
