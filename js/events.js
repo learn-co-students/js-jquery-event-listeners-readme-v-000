0f98520cc60440f3647a7002621202d69f729fe6
lawrend <douglas_lawrence@mac.com>
2017-07-30 02:11:34 -0700

    done

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+   $('p').on('click', function() {
+   alert("Hey!");
+    });
+};
 
-// call functions here
+function frameIt() {
+    $('img').addClass('tasty');
+};
+
+function pressIt() {
+    $('#typing').on('keydown', function(key) {
+        if(key.which == 71) {
+            alert("G was pressed.");
+        };
+    });
+};
+
+
+function submitIt() {
+    $('form').on('submit', function () {
+        alert("Your form is going to be submitted now.");
+    });
+};
+
+
+
+$(document).ready(function(){
+    getIt();
+
+    $('img').on('load', frameIt());
+
+    $('#typing').on('keydown', pressIt());
+
+    $('form').on('submit', submitIt());
 
 });
