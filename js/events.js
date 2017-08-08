13bca0e3701a0670366fc43753ca54850f329b70
Saad <Dropheaven@live.com>
2017-08-08 07:32:47 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt() {
+  return $('p').on("click", function () {
+    alert("Hey!")
+  });
+}
+
+function frameIt() {
+  return $('img').on("load", function () {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  return $('form').on('keydown', function (key) {
+    if (key.which === 71) {
+      alert("g was pressed")
+    }
+  });
+}
+
+function submitIt() {
+  return $("form").on("submit", function () {
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
 
 $(document).ready(function(){
 
-// call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
