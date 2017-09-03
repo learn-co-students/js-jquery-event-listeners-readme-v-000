68cd7ac68decb1ba307c4aa930c79cc8df121cbd
ode-to-the-code <justcallmethis@gmail.com>
2017-09-03 19:10:56 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,31 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+
+function getIt() {
+  $('p').on('click', function(key) {
+    alert("Hey!");
+  })
+}
+
+
+function frameIt() {
+  $('img').on('load', function(key) {
+    $('img').addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert("g was pressed");
+    }
+  })
+}
