59d29330a97dd84b561ba16b4579b4d93cc5b04c
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-06 12:55:52 -0700

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
+function getIt(){
+  $("p").on('click', function() {
+      alert("Hey!")
+    })
+}
+
+function frameIt(){
+  $("img").on('load', function() {
+    $(this).css('border', "solid 2px red").addClass('tasty')
+    })
+}
+
+function submitIt(){
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt(){
+  $("input#typing").on('keydown', function(key) {
+    if(key.which == 71){
+      alert("g was pressed.")
+    }
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+  submitIt()
+  frameIt()
+  getIt()
+  pressIt()
 
 });
