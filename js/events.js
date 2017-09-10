9eafdb98148de367ddbd7f8927845830ec592fa5
johnsontaiwo <josseytee@gmail.com>
2017-09-10 17:31:19 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,31 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert ("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('G was pressed')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+
+}
+
 
 $(document).ready(function(){
 
