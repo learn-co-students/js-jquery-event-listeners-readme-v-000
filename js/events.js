f384620cff3d638e1bc3222d4a81302dc7bbfc03
Margarita Khodzka <mkhodzka@gmail.com>
2017-09-21 03:33:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,34 @@
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+function getIt() {
+  $('p').on('click', function() {
+      alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass("tasty")
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which == 71) {
+      alert('g was pressed')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
