dd43cff07760e2bd692a3edefea140894e99e2f6
haroon Azhar Khan <haroon1azhar@gmail.com>
2017-11-11 07:08:23 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt(){
+   $("p").on('click', function() {
+       alert("Hey!")
+     })
+ }
 
-$(document).ready(function(){
+ function frameIt(){
+   $("img").on('load', function() {
+     $(this).css('border', "solid 2px red").addClass('tasty')
+     })
+ }
 
-// call functions here
+ function submitIt(){
+   $("form").on("submit", function() {
+     alert("Your form is going to be submitted now.")
+   })
+ }
 
-});
+ function pressIt(){
+   $("input#typing").on('keydown', function(key) {
+     if(key.which == 71){
+       alert("g was pressed.")
+     }
+   })
+ }
+
+  $(document).ready(function(){
+
+ // call functions here
+   submitIt()
+   frameIt()
+   getIt()
+   pressIt()
+
+  });
