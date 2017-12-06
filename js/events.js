a204e22f86627446fcdec5c1686f41154e5b9f1f
John Tuttle <thejwt@gmail.com>
2017-12-06 23:43:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function(){
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $(document).on("load", function(){
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $('form').on("keydown", function(key){
+    if (key.which == 71) {
+      alert('you have pressed the G key')
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
 
 $(document).ready(function(){
 
-// call functions here
+  frameIt();
+
+  submitIt();
 
 });
