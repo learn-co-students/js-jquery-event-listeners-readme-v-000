6fef902a565d47fe13a7666e77c8ad087efb16d3
smithwebtek <brad@smithwebtek.com>
2018-11-15 15:49:56 -0500

    Update lab

--- js/events.js
+++ js/events.js
@@ -1,33 +1,34 @@
-function getIt(){
-  $('p:first').on('click', function(){
-    alert("Hey!");
-  });
-}
 
-function frameIt(){
-  $('img:first').on('load', function(){
-    $('img:first').addClass('tasty');
-    return;
-  });
-}
-
-function pressIt(){
-  $('input#typing').on('keydown', function(e){
-    if (e.which === 71){
-      alert('You have pressed the G spot!');
-    }
-  });
-}
-
-function submitIt(){
-  $("form").on("submit", function() {
-    alert('Your form is going to be submitted now.');
-  });
-}
-
-$(document).ready(function(){
-  getIt();
-  frameIt();
-  pressIt();
-  submitIt();
+$(document).ready(function () {
+	getIt();
+	frameIt();
+	pressIt();
+	submitIt();
 });
+
+function getIt() {
+	$('p:first').on('click', function () {
+		alert("Hey!");
+	});
+}
+
+function frameIt() {
+	$('img').on('load', function () {
+		$('img').addClass('tasty');
+	});
+}
+
+function pressIt() {
+	$('#typing').on('keydown', function (e) {
+		if (e.which === 71) {
+			// if (e.key === 'g') {
+			alert('You have pressed the G key!');
+		}
+	});
+}
+
+function submitIt() {
+	$("form").on("submit", function () {
+		alert('Your form is going to be submitted now.');
+	});
+}
--- test/index-test.js
+++ test/index-test.js
@@ -1,60 +1,63 @@
 describe('Selectors', () => {
-  it('getIt() binds a click event to the paragraph', function() {
-    window.alert = expect.createSpy()
+	it('getIt() binds a click event to the paragraph', function () {
+		window.alert = expect.createSpy()
 
-    window.getIt();
+		window.getIt();
 
-    window.$('p')[0].click()
+		window.$('p')[0].click()
 
-    expect(window.alert).toHaveBeenCalled();
-  });
+		expect(window.alert).toHaveBeenCalled();
+	});
 
-  it('frameIt() binds an event that frames the minion when the page loads', function() {
-    window.frameIt()
+	it('frameIt() binds an event that frames the minion when the page loads', function () {
+		window.frameIt()
 
-    const img = window.$('img')
+		const img = window.$('img')
 
-    img.trigger('load');
+		img.trigger('load');
 
-    expect(img.attr("class")).toEqual("tasty");
-  });
+		expect(img.attr("class")).toEqual("tasty");
+	});
 
-  describe('pressIt()', () => {
-    it('pressIt() binds an event that alerts the user when the g key was pressed down', function() {
-      window.alert = expect.createSpy()
+	describe('pressIt()', () => {
+		it('pressIt() binds an event that alerts the user when the g key was pressed down', function () {
+			window.alert = expect.createSpy()
 
-      window.pressIt();
+			window.pressIt();
 
-      const input = window.$('#typing');
-      const event = window.$.Event('keydown', { which: 71 })
-      input.trigger(event)
+			const input = window.$('#typing');
+			// const event = window.$.Event('keydown', { which: 71, key: 'g' })
+			const event = window.$.Event('keydown', { which: 71 })
 
-      expect(window.alert).toHaveBeenCalled()
-    })
+			console.log('the event: ', event);
+			input.trigger(event)
 
-    it('does not alert if another key is pressed', () => {
-      window.alert = expect.createSpy()
+			expect(window.alert).toHaveBeenCalled()
+		})
 
-      window.pressIt();
+		it('does not alert if another key is pressed', () => {
+			window.alert = expect.createSpy()
 
-      const input = window.$('#typing');
-      const event = window.$.Event('keydown', { which: 70 })
-      input.trigger(event)
+			window.pressIt();
 
-      expect(window.alert).toNotHaveBeenCalled()
-    })
-  })
+			const input = window.$('#typing');
+			const event = window.$.Event('keydown', { which: 70 })
 
-  it('submitIt() binds an event that alerts "Your form is going to be submitted now." when the form is submitted', function() {
-    window.alert = expect.createSpy()
+			input.trigger(event)
+			expect(window.alert).toNotHaveBeenCalled()
+		})
+	})
 
-    window.submitIt();
+	it('submitIt() binds an event that alerts "Your form is going to be submitted now." when the form is submitted', function () {
+		window.alert = expect.createSpy()
 
-    const form = window.$('form')[0]
+		window.submitIt();
 
-    window.$(form).trigger('submit')
+		const form = window.$('form')[0]
 
-    expect(window.alert).toHaveBeenCalledWith('Your form is going to be submitted now.');
-  });
+		window.$(form).trigger('submit')
+
+		expect(window.alert).toHaveBeenCalledWith('Your form is going to be submitted now.');
+	});
 
 });
