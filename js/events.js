a724ff06c78f264427cde515a5b894e22dd4b8a3
Lamia Haider <lamia.haider@gmail.com>
2021-06-28 00:23:06 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
-//define functions here
-
-$(document).ready(function(){
-
-// call functions here
-
-});
+// bind a 'click' event (alert "Hey!") to the 'p' tag
+function getIt() {
+    $('p').on('click', function() {
+      alert('hey!')
+    })
+  }
+  
+  // bind 'load' event to image which adds 'tasty' class (adds red border)
+  function frameIt() {
+    $('img').on('load', function() {
+      $('img').addClass('tasty')
+    })
+  }
+  
+  // bind 'keydown' event to form input field that alerts when 'G' key pressed
+  function pressIt() {
+    $(document).on('keydown', function(key) {
+      if (key.which == 71)
+      alert("the 'g' key was pressed")
+    })
+  }
+  
+  // bind 'submit' event to form that alerts form is going to be submitted
+  function submitIt() {
+    $('form').on('submit', function(e) {
+      alert('Your form is going to be submitted now.')
+    })
+  }
+  
+  // call functions here
+  $(document).ready(function(){
+    getIt()
+    frameIt()
+    pressIt()
+    submitIt()
+  });
